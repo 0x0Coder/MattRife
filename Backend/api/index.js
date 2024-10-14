241f127bf296ff74857ca0ae68e9a93fa0b2c55b
anas Fred <anasfred3@gmail.com>
2024-10-14 18:41:44 +0100

    saved

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('./dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
