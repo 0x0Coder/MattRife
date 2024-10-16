83f316c5e7f9b2b251c510c119119ac0ff6936c5
anas Fred <anasfred3@gmail.com>
2024-10-16 05:49:55 +0100

    Update EventForm.jsx

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -43,7 +43,7 @@ const EventForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("http://localhost:5000/send-bookingemail", formData)
+      .post("https://mattrife-backend-72amhg0qw-0x0coders-projects.vercel.app/send-bookingemail", formData)
       .then((response) => {
         handleOpen("sm");
         console.log("Email sent successfully!");
